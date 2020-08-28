33013150aa8d628f017ce9bf0c9c8cc073bc40c8
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-08-28 16:20:42 -0700

    rb > set up __init___

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,24 @@
+# When the ring buffer is FULL and a new element is inserted
+# the OLDEST element is overwritten with the NEWEST element
+
+#  two methods, append() and get()
+
+# append
+#   adds the given element to the buffer
+
+# get 
+#   returns all of the elements in a LIST, in their given order. 
+#   Should NOT return any None values in the list even if present
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.oldest = 0 # starter & initial oldest element
+        self.storage = [] # empty List, ?
 
+    #adds the given element to the buffer
     def append(self, item):
-        pass
+        
 
     def get(self):
         pass
